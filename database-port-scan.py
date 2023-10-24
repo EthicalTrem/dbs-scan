b8fce2562ca6079293ee9534bf68f5cd8cee459b
trem <128891674+EthicalTrem@users.noreply.github.com>
2023-10-24 07:48:16 +0000

    Update database-port-scan.py
    T4  > T2

--- database-port-scan.py
+++ database-port-scan.py
@@ -3,7 +3,7 @@ import subprocess
 
 def run_nmap(target):
     try:
-        nmap_command = f'nmap -p 3306,5432,1433,1521,1526,27017,6379,5984,9042,9160,50000 -T4 -v -Pn {target}'
+        nmap_command = f'nmap -p 3306,5432,1433,1521,1526,27017,6379,5984,9042,9160,50000 -T2 -v -Pn {target}'
         subprocess.run(nmap_command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
